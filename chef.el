859a33a34ac4d4345f5729da5c039b1686a68768
Howard Abrams <howard.abrams@workday.com>
2016-05-24 21:17:15 -0700

    Calling out to openssl directly.

--- chef.el
+++ chef.el
@@ -1,6 +1,5 @@
 (require 'dash)
 (require 's)
-(require 'rsa (concat (file-name-directory (buffer-file-name)) "rsa.el"))
 
 (defun hash-it (s)
   (base64-encode-string (secure-hash 'sha1 s)))
@@ -11,6 +10,22 @@
           (mapcar (lambda (tuple) (s-join sep1 tuple))
                   (-partition 2 lst))))
 
+(defun get-string-from-file (filePath)
+  "Return filePath's file content."
+  (with-temp-buffer
+    (insert-file-contents filePath)
+    (buffer-string)))
+
+(defun openssl-it (pemfile contents)
+  (let ((infile "/tmp/blah.txt"))
+    (with-temp-file infile
+      (insert contents))
+    (with-temp-buffer
+      (toggle-enable-multibyte-characters)
+      (call-process "openssl" infile t nil
+                    "rsautl" "-sign" "-inkey" pemfile)
+      (buffer-string))))
+
 (let* ((chef-server-url "http://10.98.68.204:4000")
        (endpoint "/clients")
        (path (concat chef-server-url endpoint))
@@ -20,10 +35,11 @@
        (hashed-path (hash-it endpoint))
        (hashed-body (hash-it body))
        (timestamp (format-time-string "%Y-%m-%dT%H:%M:%SZ"))
+       (keypem (concat (getenv "HOME") "/.chef/admin.pem"))
        (canonical_request (can-it ":" "\n"
                                   (list "Method"  method
                                         "Hashed Path"  hashed-path
                                         "X-Ops-Content-Hash"  hashed-body
                                         "X-Ops-Timestamp"  timestamp
                                         "X-Ops-UserId"  user-name))))
-  canonical_request)
+  (base64-encode-string (openssl-it keypem canonical_request)))
